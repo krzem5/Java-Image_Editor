2529e5995314924138de85501ae1bf538ea7d836
Krzem <50489938+Krzem5@users.noreply.github.com>
2020-03-28 15:47:39 +0100

    Push Update

---
+++ com/krzem/image_editor/GUI.java
@@ -0,0 +1,26 @@
+package com.krzem.image_editor;
+
+
+
+import java.awt.Graphics2D;
+
+
+
+public class GUI extends Constants{
+	public void _draw(Graphics2D g){
+		g.setColor(GUI_BG_COLOR);
+		g.fillRect(0,0,WINDOW_SIZE.width,WINDOW_SIZE.height);
+	}
+
+
+
+	public void draw(Graphics2D g){
+
+	}
+
+
+
+	public void update(){
+
+	}
+}
