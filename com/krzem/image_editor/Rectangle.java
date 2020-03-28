f073f505361817580621d42b6b2ca92d53e74a29
Krzem <50489938+Krzem5@users.noreply.github.com>
2020-03-28 15:47:56 +0100

    Push Update

---
+++ com/krzem/image_editor/Rectangle.java
@@ -0,0 +1,19 @@
+package com.krzem.image_editor;
+
+
+
+public class Rectangle{
+	public int x;
+	public int y;
+	public int w;
+	public int h;
+
+
+
+	public Rectangle(int x,int y,int w,int h){
+		this.x=x;
+		this.y=y;
+		this.w=w;
+		this.h=h;
+	}
+}
