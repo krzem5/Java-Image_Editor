1c80064b40d191c751543e5111d2afba85dedd09
Krzem <krzem5.dev@gmail.com>
2021-04-10 09:32:56 +0200

    Push Update 04/10/2021, 09:32:32

--- build.bat
+++ build.bat
@@ -1,21 +1,3 @@
 @echo off
 cls
-if exist build rmdir /s /q build
-mkdir build
-cd src
-javac -d ../build com/krzem/image_editor/Main.java&&jar cvmf ../manifest.mf ../build/image_editor.jar -C ../build *&&goto run
-cd ..
-goto end
-:run
-cd ..
-pushd "build"
-for /D %%D in ("*") do (
-	rd /S /Q "%%~D"
-)
-for %%F in ("*") do (
-	if /I not "%%~nxF"=="image_editor.jar" del "%%~F"
-)
-popd
-cls
-java -jar build/image_editor.jar
-:end
+python build.py %*
